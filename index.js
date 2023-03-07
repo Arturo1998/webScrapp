2989f151e6afbf97bdb582602af16f471992a1a3
Arturo1998 <amartinvaras@gmail.com>
2023-03-07 12:22:41 +0100

    segundo commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function run () {
               return results;
               
           })
-          const pdf = await page.pdf({ format: 'A4', });
+          const pdf = await page.pdf({ format: 'A4', path:"Example.pdf" });
           browser.close();
           return resolve(urls), pdf;
       } catch (e) {
