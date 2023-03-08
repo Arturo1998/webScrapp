32d4510197f40fb5a9f08d6f69fe59eb04612ff7
Arturo1998 <amartinvaras@gmail.com>
2023-03-08 10:58:31 +0100

    forEach incorporado

--- index.js
+++ index.js
@@ -2,30 +2,35 @@ import puppeteer from 'puppeteer'
 
 const medios = [
     {nombre:"El Pais", url: "https://elpais.com/"},
-    {nombre:"El Mundo", url:"https://elmundo.es/"}
+    {nombre:"El Pais Internacional", url:"https://elpais.com/internacional/"},
+    {nombre:"El Mundo", url:"https://elmundo.es/"},
+    {nombre: "La Vanguardia", url:"https://www.lavanguardia.com/"}
 ];
 
 (async () => {
   try {
-    
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://elpais.com/')
-    const urls = await page.evaluate(() => {
-      let items = document.querySelectorAll('a')
-      let results = []
-
-      items.forEach((item) => {
-        if (item.innerText.includes('Ucrania'))
-          results.push({
-            titulo: item.innerText
-          })
+    medios.forEach(async x =>{
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto(x.url)
+      const urls = await page.evaluate(() => {
+        let items = document.querySelectorAll('a')
+        let results = []
+  
+        items.forEach((item) => {
+          if (item.innerText.includes('Victor Gil'))
+            results.push({
+              titulo: item.innerText
+            })
+        })
+        return results
       })
-      return results
+      //const pdf = await page.pdf({ format: 'A4', path:"Example.pdf" });
+      console.log(x.nombre)
+      console.log(urls)
+      await browser.close()
     })
-    //const pdf = await page.pdf({ format: 'A4', path:"Example.pdf" });
-    console.log(urls)
-    await browser.close()
+   
   } catch (e) {
     console.log(e)
   }
